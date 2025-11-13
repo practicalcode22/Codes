804ac8d65949dd809860755a4f66ecfc386f6c4c
practicalcode22 <practicalcode223@gmail.com>
2025-11-13 22:06:27 +0530

    Update priority.cpp

--- priority.cpp
+++ priority.cpp
@@ -2,69 +2,44 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-struct Process {
-    int pid, at, bt, pr, ct, tat, wt;
-};
-
+struct Process { int id, at, bt, pr, ct, tat, wt; };
 int main() {
     int n;
     cout << "Enter number of processes: ";
     cin >> n;
-
     vector<Process> p(n);
     for (int i = 0; i < n; i++) {
-        p[i].pid = i + 1;
-        cout << "\nProcess P" << p[i].pid << " Arrival Time: ";
+        p[i].id = i + 1;
+        cout << "\nP" << p[i].id << " Arrival Time: ";
         cin >> p[i].at;
         cout << "Burst Time: ";
         cin >> p[i].bt;
         cout << "Priority: ";
         cin >> p[i].pr;
     }
-
     sort(p.begin(), p.end(), [](auto &a, auto &b){ return a.at < b.at; });
-
-    int time = 0, completed = 0;
-    vector<bool> done(n, false);
-
-    while (completed < n) {
+    int time = 0, done = 0;
+    vector<bool> finished(n, false);
+    while (done < n) {
         int idx = -1, best = 1e9;
-
-        // Pick highest priority among arrived processes
-        for (int i = 0; i < n; i++) {
-            if (!done[i] && p[i].at <= time && p[i].pr < best) {
-                best = p[i].pr;
-                idx = i;
-            }
-        }
-
+        for (int i = 0; i < n; i++)
+            if (!finished[i] && p[i].at <= time && p[i].pr < best)
+                best = p[i].pr, idx = i;
         if (idx == -1) { time++; continue; }
-
         time += p[idx].bt;
-        p[idx].ct  = time;
-        p[idx].tat = p[idx].ct - p[idx].at;
-        p[idx].wt  = p[idx].tat - p[idx].bt;
-        done[idx] = true;
-        completed++;
+        p[idx].ct = time;
+        p[idx].tat = time - p[idx].at;
+        p[idx].wt = p[idx].tat - p[idx].bt;
+        finished[idx] = true;
+        done++;
     }
-
-    cout << "\n\n=== Priority Scheduling (Non-Preemptive) ===\n";
-    cout << "---------------------------------------------\n";
-    cout << "PID  AT  BT  P  CT  TAT  WT\n";
-    cout << "---------------------------------------------\n";
-
-    float ttat = 0, twt = 0;
+    float avgTAT = 0, avgWT = 0;
+    cout << "\nPID\tAT\tBT\tP\tCT\tTAT\tWT\n";
     for (auto &x : p) {
-        cout << x.pid << "   " << x.at << "   " << x.bt
-             << "   " << x.pr << "   " << x.ct
-             << "   " << x.tat << "    " << x.wt << endl;
-
-        ttat += x.tat;
-        twt += x.wt;
+        cout << x.id << "\t" << x.at << "\t" << x.bt << "\t" << x.pr << "\t"
+             << x.ct << "\t" << x.tat << "\t" << x.wt << "\n";
+        avgTAT += x.tat; avgWT += x.wt;
     }
-
-    cout << "---------------------------------------------\n";
-    cout << "Avg TAT = " << ttat / n << endl;
-    cout << "Avg WT  = " << twt / n << endl;
+    cout << "\nAverage TAT = " << avgTAT / n;
+    cout << "\nAverage WT  = " << avgWT / n << "\n";
 }
