73f049693b836cadbf2506f232d7b559760fc351
practicalcode22 <practicalcode223@gmail.com>
2025-11-14 07:41:58 +0530

    Update sjf.cpp

--- sjf.cpp
+++ sjf.cpp
@@ -1,36 +1,46 @@
-#include <stdio.h>
-typedef struct {
-    int id, at, bt, ft, tat, wt, done;
-} P;
+#include <iostream>
+using namespace std;
+
+struct P {
+    int id, at, bt, ft, tat, wt;
+    bool done;
+};
+
 int main() {
     P p[10];
     int n, t = 0, done = 0;
     float twt = 0, ttat = 0;
-    printf("Enter no. of processes: ");
-    scanf("%d", &n);
+    cout << "Enter no. of processes: ";
+    cin >> n;
+
     for (int i = 0; i < n; i++) {
-        printf("Enter AT & BT for P%d: ", i + 1);
-        scanf("%d%d", &p[i].at, &p[i].bt);
+        cout << "Enter AT & BT for P" << i + 1 << ": ";
+        cin >> p[i].at >> p[i].bt;
         p[i].id = i + 1;
-        p[i].done = 0;
+        p[i].done = false;
     }
+
     while (done < n) {
         int idx = -1, min = 9999;
         for (int i = 0; i < n; i++)
             if (!p[i].done && p[i].at <= t && p[i].bt < min)
                 min = p[i].bt, idx = i;
         if (idx == -1) { t++; continue; }
+
         p[idx].ft = t + p[idx].bt;
         p[idx].tat = p[idx].ft - p[idx].at;
         p[idx].wt = p[idx].tat - p[idx].bt;
         t = p[idx].ft;
-        p[idx].done = 1;
+        p[idx].done = true;
         done++;
         twt += p[idx].wt;
         ttat += p[idx].tat;
     }
-    printf("\nPID\tAT\tBT\tFT\tTAT\tWT\n");
+
+    cout << "\nPID\tAT\tBT\tFT\tTAT\tWT\n";
     for (int i = 0; i < n; i++)
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].id, p[i].at, p[i].bt, p[i].ft, p[i].tat, p[i].wt);
-    printf("\nAvg WT: %.2f\nAvg TAT: %.2f\n", twt / n, ttat / n);
+        cout << p[i].id << "\t" << p[i].at << "\t" << p[i].bt << "\t"
+             << p[i].ft << "\t" << p[i].tat << "\t" << p[i].wt << "\n";
+
+    cout << "\nAvg WT: " << twt / n << "\nAvg TAT: " << ttat / n << "\n";
 }
